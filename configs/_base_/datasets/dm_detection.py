fd53592d558c5aed6151d658067d02f54e0382b3
Sondre Nesset <sondrern@gmail.com>
2022-05-29 21:02:00 +0200

    Updated conf IMG sizes

--- configs/_base_/datasets/dm_detection.py
+++ configs/_base_/datasets/dm_detection.py
@@ -3,13 +3,13 @@ from __future__ import division, print_function, absolute_import
 
 DATASET_NAME = 'DM'
 CLASS_NUM = 1
-'''
+
 PIXEL_MEAN = [123.68, 116.779, 103.939]  # R, G, B. In tf, channel is RGB. In openCV, channel is BGR
 PIXEL_MEAN_ = [0.485, 0.456, 0.406]
 PIXEL_STD = [0.229, 0.224, 0.225]
 IMG_SHORT_SIDE_LEN = 800
 IMG_MAX_LENGTH = 800
-'''
+
 # data augmentation
 IMG_ROTATE = False
 RGB2GRAY = False
